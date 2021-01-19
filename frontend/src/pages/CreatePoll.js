22316649b9cb78ec7d40af06cf91e7e36ee4926f
Karan Mann <62376840+karanmann@users.noreply.github.com>
2021-01-19 17:05:09 +0100

    Dummy data and visual added

--- frontend/src/pages/CreatePoll.js
+++ frontend/src/pages/CreatePoll.js
@@ -1,18 +1,34 @@
 import React from 'react'
 
+import { Table } from '../components/Table'
+
 export const CreatePoll= () => {
   return (
     <>
-    <section>
-      <h1>Create poll</h1>
-      <form>
-        <label>
-         <input type='text'/>
-        </label>
-        </label>
-        </label>
-      </form>
-    </section>
+      <section>
+        <h1>Create poll</h1>
+        <form>
+          <label>
+          <input type='text'/>
+          Topic
+          </label>
+        </form>
+      </section>
+      <section>
+        <h1>Add options</h1>
+        <form>
+          <input type='text' />
+          <button>Add</button>
+          <button>Back</button>
+          <button>Create poll and see summary</button>
+        </form>
+      </section>
+      <section>
+        <h1>Summary</h1>
+        <Table />
+        <button>Go back</button>
+        <button>Finish and create link</button>
+      </section>
     </>
   )
 }
