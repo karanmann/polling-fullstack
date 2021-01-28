a27c616759bc575b6c9f0e51a64dcf10963059c7
Karan Mann <62376840+karanmann@users.noreply.github.com>
2021-01-28 14:22:28 +0100

    Generate Link Button

--- frontend/src/pages/PollingLink.js
+++ frontend/src/pages/PollingLink.js
@@ -10,11 +10,17 @@ import { poll } from '../reducer/poll'
 export const PollingLink= () => {
   const pollId = useSelector((store) => store.poll.pollId) // change if we solve the nesting problem
   const url = `http://localhost:3000/voting/${pollId}`
+
+  const copyLink = () => {
+    document.getElementById('linkUrl').select()
+    document.execCommand('copy')
+  }
+
   return (
     <Container>
       <HeaderPoll>Tadaa your link</HeaderPoll>
-      <p>{url}</p>
-      <Button variant='contained' color='secondary'>Copy Link</Button>
+      <input id="linkUrl" defaultValue={url}></input>
+      <Button variant='contained' color='secondary' onClick={() => copyLink()}>Copy Link</Button>
       <Link to='/'>
         <Button variant='contained' color='primary'>Back to Start Page</Button>
       </Link>
