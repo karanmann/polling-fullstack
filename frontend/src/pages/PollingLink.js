0396ae378f61f764fdfd1b2cd1aaccef69cc83fe
Ingela Löfgren <ingela.lofgren@gmail.com>
2021-01-28 14:24:59 +0100

    Added resetState in Poling link

--- frontend/src/pages/PollingLink.js
+++ frontend/src/pages/PollingLink.js
@@ -3,20 +3,28 @@ import { Link, useParams } from 'react-router-dom'
 import { Button } from '@material-ui/core'
 
 import {  HeaderPoll, Container } from '../lib/Styling'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { poll } from '../reducer/poll'
 
 export const PollingLink= () => {
+  const dispatch = useDispatch()
   const pollId = useSelector((store) => store.poll.pollId) // change if we solve the nesting problem
   const url = `http://localhost:3000/voting/${pollId}`
+
+  const resetState = () => {
+    dispatch(poll.actions.setPollTopic({ topic: ''} ))
+    dispatch(poll.actions.setPollId(null))
+    dispatch(poll.actions.setPollOptions({options: '' }))
+  }
+
   return (
     <Container>
       <HeaderPoll>Tadaa your link</HeaderPoll>
       <p>{url}</p>
       <Button variant='contained' color='secondary'>Copy Link</Button>
       <Link to='/'>
-        <Button variant='contained' color='primary'>Back to Start Page</Button>
+        <Button onClick={resetState} variant='contained' color='primary'>Back to Start Page</Button>
       </Link>
     </Container>
   )
